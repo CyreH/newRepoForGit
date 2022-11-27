02aaefeeb94f3ece48874259c7a4789a6184e62f
Gusamov Artur <arturgusamov@mail.ru>
2022-11-28 00:37:04 +0500

    unite table and statistics programs

--- Statistics.py
+++ Statistics.py
@@ -5,7 +5,38 @@ import openpyxl
 from openpyxl.styles import Font, Border, Side
 import pdfkit
 from jinja2 import Environment, FileSystemLoader
+import prettytable
+from prettytable import PrettyTable
+import re
 
+tax = {
+    "True": "Без вычета налогов",
+    "False": "С вычетом налогов"
+}
+translation = ['Название', 'Описание', 'Навыки', 'Опыт работы', 'Премиум-вакансия', 'Компания', 'Оклад',
+               'Название региона', 'Дата публикации вакансии']
+bools = {
+    "True": "Да",
+    "False": "Нет"
+}
+experience = {
+    "noExperience": "Нет опыта",
+    "between1And3": "От 1 года до 3 лет",
+    "between3And6": "От 3 до 6 лет",
+    "moreThan6": "Более 6 лет"
+}
+currencies = {
+    "AZN": "Манаты",
+    "BYR": "Белорусские рубли",
+    "EUR": "Евро",
+    "GEL": "Грузинский лари",
+    "KGS": "Киргизский сом",
+    "KZT": "Тенге",
+    "RUR": "Рубли",
+    "UAH": "Гривны",
+    "USD": "Доллары",
+    "UZS": "Узбекский сум"
+}
 currency_to_rub = {
     "AZN": 35.68,
     "BYR": 23.91,
@@ -20,6 +51,36 @@ currency_to_rub = {
 }
 
 
+def sort_exp(vac):
+    s = re.findall(r'\d*\.\d+|\d+', vac.experience_id)
+    return 0 if len(s) == 0 else int(s[0])
+
+
+sorts = {
+    'Название': lambda vac, is_rev: vac.sort(reverse=is_rev, key=lambda vac: vac.name),
+    'Описание': lambda vac, is_rev: vac.sort(reverse=is_rev, key=lambda vac: vac.description),
+    'Навыки': lambda vac, is_rev: vac.sort(reverse=is_rev, key=lambda vac: len(vac.key_skills)),
+    'Опыт работы': lambda vac, is_rev: vac.sort(reverse=is_rev, key=lambda vac: sort_exp(vac)),
+    'Премиум-вакансия': lambda vac, is_rev: vac.sort(reverse=is_rev, key=lambda vac: vac.premium),
+    'Компания': lambda vac, is_rev: vac.sort(reverse=is_rev, key=lambda vac: vac.employer_name),
+    'Оклад': lambda vac, is_rev: vac.sort(reverse=is_rev, key=lambda vac: vac.salary.salary_currency_to_ruble),
+    'Название региона': lambda vac, is_rev: vac.sort(reverse=is_rev, key=lambda vac: vac.area_name),
+    'Дата публикации вакансии': lambda vac, is_rev: vac.sort(reverse=is_rev, key=lambda vac: vac.published_at)
+}
+functions = {
+    'Название': lambda vac, val: vac.name == val,
+    'Опыт работы': lambda vac, val: vac.experience_id == val,
+    'Описание': lambda vac, val: vac.description == val,
+    'Дата публикации вакансии': lambda vac, val: f'{vac.published_at[8:10]}.{vac.published_at[5:7]}.{vac.published_at[:4]}' == val,
+    'Премиум-вакансия': lambda vac, val: vac.premium == val,
+    'Название региона': lambda vac, val: vac.area_name == val,
+    'Компания': lambda vac, val: vac.employer_name == val,
+    'Идентификатор валюты оклада': lambda vac, val: vac.salary.salary_currency == val,
+    'Навыки': lambda vac, val: all(x in vac.key_skills for x in val.split(', ')),
+    'Оклад': lambda vac, val: float(vac.salary.salary_from) <= float(val) <= float(vac.salary.salary_to)
+}
+
+
 class Vacancy:
     def __init__(self, vac):
         self.name = vac['name']
@@ -30,6 +91,23 @@ class Vacancy:
         self.published_at = vac['published_at']
         self.year = self.published_at[:4]
         self.salary = (float(self.salary_from) + float(self.salary_to)) / 2 * currency_to_rub[self.salary_currency]
+        if len(vac) > 6:
+            self.employer_name = vac['employer_name']
+            self.description = vac['description']
+            self.key_skills = vac['key_skills'].split('_')
+            self.experience_id = experience[vac['experience_id']]
+            self.premium = bools[vac['premium']]
+            self.salary_gross = bools[vac['salary_gross']]
+
+    def make_salary(self):
+        sal_from = '{0:,}'.format(int(float(self.salary_from))).replace(',', ' ')
+        sal_to = '{0:,}'.format(int(float(self.salary_to))).replace(',', ' ')
+        return f'{sal_from} - {sal_to} ({self.salary_currency}) ({self.salary_gross})'
+
+    def __str__(self):
+        return [self.name, self.description, '\n'.join(self.key_skills), self.experience_id, self.premium,
+                self.employer_name, self.make_salary(), self.area_name,
+                f'{self.published_at[8:10]}.{self.published_at[5:7]}.{self.published_at[:4]}']
 
 
 class DataSet:
@@ -45,11 +123,19 @@ class DataSet:
         self.amount_by_city = {}
 
     @staticmethod
+    def strRefactor(str):
+        str = re.sub(r"<[^>]*>", '', str)
+        str = str.replace('\n', '_')
+        str = ' '.join(str.split())
+        return str
+
+    @staticmethod
     def CSV_parser(csv_file):
         file = open(csv_file, encoding='utf-8-sig')
         csv_reader = csv.reader(file)
         titles = next(csv_reader)
-        vacancies = [dict(zip(titles, x)) for x in csv_reader if '' not in x and len(x) == len(titles)]
+        vacancies = [dict(zip(titles, [DataSet.strRefactor(s) for s in x])) for x in csv_reader if
+                     '' not in x and len(x) == len(titles)]
         return vacancies
 
     @staticmethod
@@ -97,6 +183,95 @@ class DataSet:
         self.amount_by_city = dict(sorted(self.amount_by_city.items(), key=lambda val: val[1], reverse=True)[:10])
 
 
+class Table:
+    def __init__(self, f_name):
+        self.f_name = f_name
+        self.filter = input('Введите параметр фильтрации: ')
+        self.sort_type = input('Введите параметр сортировки: ')
+        self.is_rev_sort = input('Обратный порядок сортировки (Да / Нет): ')
+        self.boarders = input('Введите диапазон вывода: ')
+        self.need_titles = input('Введите требуемые столбцы: ')
+        self.filter = self.param_fixer(self.filter, 'param')
+        self.sort_type = self.param_fixer(self.sort_type, 'sort')
+        self.is_rev_sort = self.param_fixer(self.is_rev_sort, 'rev')
+
+        self.vacancies = DataSet(self.f_name)
+        self.titles = translation
+
+        self.boarders = self.boarders.split() if self.boarders else '0'
+        self.need_titles = self.need_titles.split(', ') if self.need_titles else 'all'
+
+    @staticmethod
+    def param_fixer(p, type):
+        if type == 'param':
+            if not p:
+                return 'nothing'
+            if ':' not in p:
+                print('Формат ввода некорректен')
+                quit()
+            p = p.split(': ')
+            if p[0] not in translation and p[0] != 'Идентификатор валюты оклада':
+                print('Параметр поиска некорректен')
+                quit()
+        if type == 'sort':
+            if not p:
+                return 'nothing'
+            if p not in translation:
+                print('Параметр сортировки некорректен')
+                quit()
+        if type == 'rev':
+            if p not in ['Да', 'Нет', '']:
+                print('Порядок сортировки задан некорректно')
+                quit()
+        return p
+
+    def get_filtered(self, vac):
+        if self.filter == 'nothing':
+            return vac
+        return functions[self.filter[0]](vac, self.filter[1])
+
+    def sort_vac(self):
+        if self.sort_type == 'nothing':
+            return self.vacancies.vacancies_objects
+        is_reverse = True if self.is_rev_sort == 'Да' else False
+        sorts[self.sort_type](self.vacancies.vacancies_objects, is_reverse)
+        return self.vacancies.vacancies_objects
+
+    def print_table(self):
+        table = PrettyTable()
+        titles = self.titles.copy()
+        titles.insert(0, '№')
+        table.field_names = titles
+        table.max_width = 20
+        table.hrules = prettytable.ALL
+        table.align = "l"
+
+        if len(self.boarders) == 1:
+            start = int(self.boarders[0]) - 1 if int(self.boarders[0]) != 0 else 0
+            end = len(self.vacancies.vacancies_objects)
+        else:
+            start = int(self.boarders[0]) - 1 if int(self.boarders[0]) != 0 else 0
+            end = int(self.boarders[1]) - 1 if int(self.boarders[1]) != 0 else 0
+        if self.need_titles == 'all':
+            self.need_titles = titles
+        self.need_titles.insert(0, '№')
+        i = 0
+        for vac in self.sort_vac():
+            row = vac.__str__()
+            if not self.get_filtered(vac):
+                continue
+            for r in range(len(row)):
+                if len(row[r]) > 100:
+                    row[r] = f"{row[r][:100]}..."
+            row.insert(0, i + 1)
+            table.add_row(row)
+            i += 1
+        if i == 0:
+            print('Ничего не найдено')
+        else:
+            print(table.get_string(start=start, end=end, fields=self.need_titles))
+
+
 class Report:
     def __init__(self, f_name, prof_name):
         self.f_name = f_name
@@ -106,6 +281,15 @@ class Report:
         self.workbook = openpyxl.Workbook()
         self.years = [x for x in self.data_set.sal_by_years]
         self.fig, self.ax = plt.subplots(nrows=2, ncols=2)
+
+    def print_data(self):
+        print('Динамика уровня зарплат по годам:', self.data_set.sal_by_years)
+        print('Динамика количества вакансий по годам:', self.data_set.amount_by_years)
+        print('Динамика уровня зарплат по годам для выбранной профессии:', self.data_set.sal_by_years_for_prof)
+        print('Динамика количества вакансий по годам для выбранной профессии:',
+              self.data_set.amount_prof_by_years)
+        print('Уровень зарплат по городам (в порядке убывания):', self.data_set.sal_by_city)
+        print('Доля вакансий по городам (в порядке убывания):', self.data_set.amount_by_city)
 
     def generate_pdf(self):
         headers1, headers2, headers3, t1_data, t2_data, t3_data = self.make_data()
@@ -263,21 +447,18 @@ class Report:
 
 class Input:
     def __init__(self):
+        self.request = input('Вывести вакансии или статистику?')
         # self.f_name = input('Введите название файла: ')
-        # self.prof_name = input('Введите название профессии: ')
-        self.f_name = 'vac.csv'
-        self.prof_name = 'Программист'
-        self.report = Report(self.f_name, self.prof_name)
-
-    def print(self):
-        print('Динамика уровня зарплат по годам:', self.report.data_set.sal_by_years)
-        print('Динамика количества вакансий по годам:', self.report.data_set.amount_by_years)
-        print('Динамика уровня зарплат по годам для выбранной профессии:', self.report.data_set.sal_by_years_for_prof)
-        print('Динамика количества вакансий по годам для выбранной профессии:',
-              self.report.data_set.amount_prof_by_years)
-        print('Уровень зарплат по городам (в порядке убывания):', self.report.data_set.sal_by_city)
-        print('Доля вакансий по городам (в порядке убывания):', self.report.data_set.amount_by_city)
+        self.f_name = 'vacancies_big.csv'
+        if self.request.casefold()[:4] == 'стат':
+            # self.prof_name = input('Введите название профессии: ')
+            self.prof_name = 'Программист'
+            self.report = Report(self.f_name, self.prof_name)
+            self.report.generate_pdf()
+            self.report.print_data()
+        else:
+            self.table = Table(self.f_name)
+            self.table.print_table()
 
 
 x = Input()
-x.report.generate_pdf()
